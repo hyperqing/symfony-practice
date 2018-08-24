b77a275009fa5249f63a5d022dcebfeb6a2fb163
hyperqing <469379004@qq.com>
2018-08-24 15:57:52 +0800

    改进示例

--- asset/component/AddRuleDialog.vue
+++ asset/component/AddRuleDialog.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="content">
         <el-button @click="dialogVisible = true">
-            显示对话框
+            上传文件
         </el-button>
         <el-dialog
                 title="提示"
                 :visible.sync="dialogVisible"
                 width="30%">
-            <span>{{ msg }}</span>
+            <el-upload
+                    class="upload-demo"
+                    action="https://jsonplaceholder.typicode.com/posts/"
+                    :on-preview="handlePreview"
+                    :on-remove="handleRemove"
+                    :before-remove="beforeRemove"
+                    multiple
+                    :limit="3"
+                    :on-exceed="handleExceed"
+                    :file-list="fileList">
+                <el-button size="small" type="primary">点击上传</el-button>
+                <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+            </el-upload>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="dialogVisible = false">取 消</el-button>
                 <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
@@ -21,8 +33,22 @@
         name: "AddRuleDialog",
         data() {
             return {
-                msg: "添加规则对话框",
-                dialogVisible: false
+                dialogVisible: false,
+                fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+            }
+        },
+        methods: {
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePreview(file) {
+                console.log(file);
+            },
+            handleExceed(files, fileList) {
+                this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+            },
+            beforeRemove(file, fileList) {
+                return this.$confirm(`确定移除 ${ file.name }？`);
             }
         }
     }
