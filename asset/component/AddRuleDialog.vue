6a07087600a49d5357da2a9013766b6c9562295f
hyperqing <469379004@qq.com>
2018-08-24 15:07:16 +0800

    添加对话框

--- asset/component/AddRuleDialog.vue
+++ asset/component/AddRuleDialog.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="content">
-        {{ msg }}
+        <el-button @click="dialogVisible = true">
+            显示对话框
+        </el-button>
+        <el-dialog
+                title="提示"
+                :visible.sync="dialogVisible"
+                width="30%">
+            <span>{{ msg }}</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -9,7 +21,8 @@
         name: "AddRuleDialog",
         data() {
             return {
-                msg: "添加规则对话框"
+                msg: "添加规则对话框",
+                dialogVisible: false
             }
         }
     }
